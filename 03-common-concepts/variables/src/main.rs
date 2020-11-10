d22d8569ff5a87fb334a7b39d653f779de6bf850
Kris Pritchard <krp@users.noreply.github.com>
2020-11-11 01:08:38 +1300

    Add 03-variables

---
+++ 03-common-concepts/variables/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x: f32 = 1.0;
+
+    const max_pOiNts: i32 = 1_0_0_0_0;
+
+    println!("x = {}", x);
+
+    let x = 5;
+
+    println!("x = {}", x);
+
+    let x = "hello";
+
+    println!("x = {}", x);
+
+    println!("max points: {}", max_pOiNts);
+
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("spaces: {}", spaces);
+}
